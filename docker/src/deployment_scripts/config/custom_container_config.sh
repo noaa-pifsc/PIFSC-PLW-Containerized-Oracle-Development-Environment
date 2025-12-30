2fd03ac0838e2785371cdb163a54e187ce2f300f
Jesse Abdul <jesse.abdul@noaa.gov>
2025-12-30 09:12:25 -1000

    --PIFSC PLW Containerized Oracle Developer Environment: Upgraded Containers--
    	Synced with Version 1.3 (Git tag: CODE_v1.3) of the upstream project Containerized Oracle Developer Environment (CODE) project (Git URL: git@github.com:noaa-pifsc/PIFSC-Containerized-Oracle-Development-Environment.git)
    
    	Adding standard github requirements
    
    	Implemented dependencies as git submodules
    
    	Defined custom configuration and secret files for PLW project

--- docker/src/deployment_scripts/config/custom_container_config.sh
+++ docker/src/deployment_scripts/config/custom_container_config.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
 # define any database/apex credentials necessary to deploy the database schemas and/or applications
+
+
+# define PLW schema credentials
+DB_PICLIB_USER="PICLIB"
+DB_PLW_PASSWORD="[CONTAINER_PW]"
+
+# define PLW connection string
+PICLIB_CREDENTIALS="$DB_PICLIB_USER/$DB_PLW_PASSWORD@${DBHOST}:${DBPORT}/${DBSERVICENAME}"
+
+# define the PLW database folder path
+PLW_FOLDER_PATH="/usr/src/PLW/SQL"
+
+# PLW app username
+DB_PLW_USER="PUB_RPTS"
+
+# define PLW app connection string
+PLW_CREDENTIALS="$DB_PLW_USER/$DB_PLW_PASSWORD@${DBHOST}:${DBPORT}/${DBSERVICENAME}"
